058913b2b50ee548a6924dc525677596d63e48b4
Gustavo Carmona <p8queen@gmail.com>
2023-03-10 22:00:37 -0300

    response json
    server response with json files, after the files have been readed with require.

--- server.js
+++ server.js
@@ -1,13 +1,47 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req,res) => {
     console.log('A request had been made');
     console.log(req.url, req.method);
 
     //send response
-    res.setHeader('Content-Type', 'text/plain');
-    res.write('Hello NodeJs');
-    res.end();
+    res.setHeader('Content-Type', 'application/json');
+    let path = './views/'
+    switch(req.url) {
+        case '/':
+            path += 'index.json';
+            break;
+        case '/about':
+            path += 'about.json';
+            break;
+        default:
+            path += '404.json';
+            break;
+    }
+    //send the file
+    console.log('path: ', path);
+    const data = require(path);
+    console.log(data);
+    try {
+        res.end(JSON.stringify(data));    
+    }catch (e){
+        console.log('error res.end()');
+    }
+
+
+    // fs.readFile(path, (err,data) => {
+    //     if(err){
+    //         console.log(err);
+    //         res.end();
+    //         }else{
+    //             const obj = JSON.parse(data.toString());
+    //             const jsonData = JSON.stringify(obj);
+    //             res.end(jsonData);
+                
+    //         }
+    // }); // readfile
+    
 });
 
 server.listen(3000, 'localhost', () => {
